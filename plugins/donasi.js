bf4ab3179b5137a1e08d57a562166444d75287d7
ᴹᴿᤎ KhairilOfcི ࿇ <90993998+Khairil-Fnt@users.noreply.github.com>
2022-06-25 20:27:01 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,8 +1,8 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+╭─「 Donasi • Kredit 」
+│ •  Digi (60109102974
 ╰────
+© Khairil春
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
